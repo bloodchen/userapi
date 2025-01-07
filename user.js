9a3b5e3ecde4af6b780a5c1583689c0f021b0f0d
bloodchen <bloodchen@gmail.com>
2025-01-07 09:46:43 +0800

    update

--- user.js
+++ user.js
@@ -11,8 +11,6 @@ export class User extends BaseService {
             this.pname = config.project.name || 'userapi'
             this.db = this.client.db(this.pname);
             gl.mongo = this.client
-<<<<<<< HEAD
-=======
             if (stripeTesting) {
                 this.endSecret = process.env.stripe_sec_test//mx testing
                 stripe = Stripe(process.env.stripe_key_test)
@@ -21,7 +19,7 @@ export class User extends BaseService {
                 stripe = Stripe(process.env.stripe_key)
             }
             gl.stripe = stripe
->>>>>>> 4bcac5d (update)
+
         } catch (e) {
             console.error("MongoClient error:", e.message)
         }
